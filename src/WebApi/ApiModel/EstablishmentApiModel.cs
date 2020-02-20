c98cfbee7a4c5ff7bd10fdb2d34db5ccb63ff08f
Douglas Ramos <douglas.dcr@outlook.com>
2020-02-20 18:55:45 -0300

    Projects documentation improvement 🧩

---
+++ src/WebApi/ApiModel/EstablishmentApiModel.cs
@@ -0,0 +1,24 @@
+﻿using Plannoy.Domain;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Plannoy.WebApi
+{
+    public class EstablishmentApiModel
+    {
+        /// <summary>
+        /// Establishment's name
+        /// </summary>
+        [Required]
+        public string Name { get; set; } = null!;
+
+        /// <summary>
+        /// Establishment's sector
+        /// </summary>
+        [Required]
+        public string Sector { get; set; } = null!;
+    }
+}
--- src/WebApi/ApiModel/TransactionApiModel.cs
+++ src/WebApi/ApiModel/TransactionApiModel.cs
@@ -5,24 +5,38 @@ using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
-namespace WebApi
+namespace Plannoy.WebApi
 {
     public class TransactionApiModel
     {
         /// <summary>
-        /// Name of establishment
+        /// Reference Date
         /// </summary>
+        [Required]
         public DateTime ReferenceDate { get; set; }
 
         /// <summary>
-        /// Establishment on which the transaction was made
+        /// Payment Method
         /// </summary>
-        public Establishment? Establishment { get; set; }
-
+        [Required]
         public TransactionPaymentMethod PaymentMethod { get; set; }
 
+        /// <summary>
+        /// Value of the transacation
+        /// </summary>
+        [Required]
         public decimal Value { get; set; }
 
+        /// <summary>
+        /// Currency use in the transacation
+        /// </summary>
+        [Required]
         public Currency Currency { get; set; }
+
+        /// <summary>
+        /// Name of the establishment where the transaction occured
+        /// </summary>
+        [Required]
+        public string Establishment { get; set; } = null!;
     }
 }
